9e5cb6f2307cc6b9168283ebc408c47c292d7126
Alma Rozanès <arozane.ext@groupe-beaumanoir.fr>
2025-01-18 13:35:38 +0100

    feat(recipe): collapsible ingredients

--- components/recipe/view/RecipeIngredients.vue
+++ components/recipe/view/RecipeIngredients.vue
@@ -1,54 +1,113 @@
 <template>
-  <div class="flex min-w-56 basis-56 flex-col gap-4">
+  <div class="flex min-w-56 basis-56 flex-col gap-6">
     <h3 class="heading-3">Ingredients</h3>
-    <ul
-      class="sticky top-0 grid grid-cols-[1fr_auto_auto_auto] gap-x-1 gap-y-2"
-    >
-      <li
-        v-for="ingredient in recipe.ingredients"
-        :key="ingredient.id"
-        class="col-span-4 grid grid-cols-subgrid pb-1"
-        :class="'separate' in ingredient ? '' : 'border-b'"
+
+    <div class="sticky top-0 flex flex-col gap-6">
+      <CollapsibleRoot
+        v-for="group in groupedBySeparators"
+        :key="group.id"
+        v-slot="{ open }"
+        class="group/collapsible flex flex-col gap-3"
+        default-open
       >
-        <template v-if="'separate' in ingredient">
-          <div class="mb-1 mt-6 text-xs uppercase text-muted-foreground">
-            {{ ingredient.separate }}
+        <CollapsibleTrigger
+          v-if="group.label"
+          class="flex items-center justify-between"
+        >
+          <div class="text-xs uppercase text-muted-foreground">
+            {{ group.label }}
           </div>
-        </template>
-        <template v-else>
-          <div class="">
-            {{ ingredient.name }}
-          </div>
-          <div />
-          <div class="justify-self-end">
-            {{
-              ingredient.quantity &&
-              formatter.formatQuantity(ingredient.quantity, ingredient.unit)
-            }}
-          </div>
-          <span class="text-gray-400">
-            {{ ingredient.unit && formatter.formatUnit(ingredient.unit) }}
-          </span>
-          <div
-            v-if="ingredient.notes"
-            class="col-span-3 text-xs text-muted-foreground"
+          <ChevronsUpDown
+            class="size-3.5 stroke-muted-foreground opacity-0 transition-opacity group-hover/collapsible:opacity-100"
+          />
+        </CollapsibleTrigger>
+        <CollapsibleContent
+          class="grid grid-cols-[1fr_auto_auto_auto] gap-x-1 gap-y-2"
+        >
+          <li
+            v-for="ingredient in group.ingredients"
+            :key="ingredient.id"
+            class="col-span-4 grid grid-cols-subgrid pb-1"
+            :class="'separate' in ingredient ? '' : 'border-b'"
           >
-            {{ ingredient.notes }}
-          </div>
-        </template>
-      </li>
-    </ul>
+            <div class="">
+              {{ ingredient.name }}
+            </div>
+            <div />
+            <div class="justify-self-end">
+              {{
+                ingredient.quantity &&
+                formatter.formatQuantity(ingredient.quantity, ingredient.unit)
+              }}
+            </div>
+            <span class="text-gray-400">
+              {{ ingredient.unit && formatter.formatUnit(ingredient.unit) }}
+            </span>
+            <div
+              v-if="ingredient.notes"
+              class="col-span-3 text-xs text-muted-foreground"
+            >
+              {{ ingredient.notes }}
+            </div>
+          </li>
+        </CollapsibleContent>
+      </CollapsibleRoot>
+    </div>
   </div>
 </template>
 <script setup lang="ts">
 import { UnitFormatter } from "~/lib/Unit";
-import type { RecipeDetails } from "~/types/recipe";
+import type { Ingredient, RecipeDetails } from "~/types/recipe";
+import { v4 } from "uuid";
+import { initial, last } from "lodash";
+import {
+  CollapsibleContent,
+  CollapsibleRoot,
+  CollapsibleTrigger,
+} from "radix-vue";
+import { ChevronsUpDown } from "lucide-vue-next";
 
-defineProps<{
+const props = defineProps<{
   recipe: Pick<RecipeDetails, "ingredients">;
 }>();
 
 const formatter = new UnitFormatter({
   style: "short",
 });
+
+const groupedBySeparators = computed(() =>
+  props.recipe.ingredients.reduce(
+    (acc, val) => {
+      if ("separate" in val) {
+        return [
+          ...acc,
+          {
+            id: val.id,
+            label: val.separate,
+            ingredients: [],
+          },
+        ];
+      }
+
+      if (acc.length === 0) {
+        return [
+          {
+            id: v4(),
+            ingredients: [val],
+          },
+        ];
+      }
+      const lastElement = last(acc)!;
+
+      return [
+        ...initial(acc),
+        {
+          ...lastElement,
+          ingredients: [...lastElement.ingredients, val],
+        },
+      ];
+    },
+    [] as { id: string; label?: string; ingredients: Ingredient[] }[],
+  ),
+);
 </script>
