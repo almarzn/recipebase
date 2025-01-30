b9a9dc792c68c251e32de0b52ea121447e4bb258
Alma Rozanès <arozane.ext@groupe-beaumanoir.fr>
2025-01-30 15:04:09 +0100

    chore: add pagination component

---
+++ components/ui/pagination/PaginationEllipsis.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { cn } from "@/lib/utils";
+import { PaginationEllipsis, type PaginationEllipsisProps } from "radix-vue";
+import { computed, type HTMLAttributes } from "vue";
+import { EllipsisIcon } from "lucide-vue-next";
+
+const props = defineProps<
+  PaginationEllipsisProps & { class?: HTMLAttributes["class"] }
+>();
+
+const delegatedProps = computed(() => {
+  const { class: _, ...delegated } = props;
+
+  return delegated;
+});
+</script>
+
+<template>
+  <PaginationEllipsis
+    v-bind="delegatedProps"
+    :class="cn('flex h-9 w-9 items-center justify-center', props.class)"
+  >
+    <slot>
+      <EllipsisIcon />
+    </slot>
+  </PaginationEllipsis>
+</template>
