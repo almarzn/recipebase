28acbeecc50d7f1dba38fa07c3a829cd56acd90d
almarzn <113595328+almarzn@users.noreply.github.com>
2026-05-02 23:34:52 +0200

    style: move all toolbar buttons to the end

--- frontend/src/app/features/recipes/recipe-edit/components/editor-ingredient.component.ts
+++ frontend/src/app/features/recipes/recipe-edit/components/editor-ingredient.component.ts
@@ -42,6 +42,26 @@ import type { EditableIngredient } from "./recipe-component-editor.vm";
       </div>
 
       <div class="flex items-center gap-1" data-testid="ingredient-toolbar">
+        <span class="flex-1"></span>
+        <button
+          z-button
+          zType="ghost"
+          zSize="icon-xs"
+          (click)="toggleNotes()"
+          [class.border-primary!]="hasNotes()"
+          data-testid="ingredient-toggle-notes"
+        >
+          <ng-icon name="lucideNotebookPen" [class.text-primary!]="hasNotes()" />
+        </button>
+        <button
+          z-button
+          zType="ghost"
+          zSize="icon-xs"
+          (click)="delete.emit()"
+          data-testid="ingredient-delete"
+        >
+          <ng-icon name="lucideTrash" />
+        </button>
         <button
           z-button
           zType="ghost"
@@ -61,25 +81,6 @@ import type { EditableIngredient } from "./recipe-component-editor.vm";
           data-testid="ingredient-move-down"
         >
           <ng-icon name="lucideChevronDown" />
-        </button>
-        <button
-          z-button
-          zType="ghost"
-          zSize="icon-xs"
-          (click)="toggleNotes()"
-          [class.border-primary!]="hasNotes()"
-          data-testid="ingredient-toggle-notes"
-        >
-          <ng-icon name="lucideNotebookPen" [class.text-primary!]="hasNotes()" />
-        </button>
-        <button
-          z-button
-          zType="ghost"
-          zSize="icon-xs"
-          (click)="delete.emit()"
-          data-testid="ingredient-delete"
-        >
-          <ng-icon name="lucideTrash" />
         </button>
       </div>
 
